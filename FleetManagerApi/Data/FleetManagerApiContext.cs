d74fec68cecf0a119480e3ae67465ee4d6fda2cc
Bryan <bryandosborne@gmail.com>
2026-06-18 16:07:08 -0400

    Added OnModelCreating()

--- FleetManagerApi/Data/FleetManagerApiContext.cs
+++ FleetManagerApi/Data/FleetManagerApiContext.cs
@@ -8,5 +8,13 @@ namespace FleetManagerApi.Data
         public required DbSet<Truck> Trucks { get; set; }
         public required DbSet<Driver> Drivers { get; set; }
         public required DbSet<Load> Loads { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);            
+            modelBuilder.Entity<Driver>().Property(d => d.FirstName).HasMaxLength(50);
+            modelBuilder.Entity<Driver>().Property(d => d.LastName).HasMaxLength(50);
+            modelBuilder.Entity<Truck>().Property(t => t.TruckNumber).HasMaxLength(20);
+        }
     }
 }
---
+++ FleetManagerApi/Migrations/20260618200100_OnModelCreating.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FleetManagerApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class OnModelCreating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TruckNumber",
+                table: "Trucks",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Drivers",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Drivers",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TruckNumber",
+                table: "Trucks",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Drivers",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Drivers",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
